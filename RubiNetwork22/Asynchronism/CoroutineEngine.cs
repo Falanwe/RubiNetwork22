c4bb34ad64e3eebd69b7b893add05bc450d8e22b
Jean-Noël Gourdol <jngourdol@stormancer.com>
2022-03-29 11:46:59 +0200

    ajout d'un sample de coroutines

---
+++ RubiNetwork22/Asynchronism/CoroutineEngine.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Asynchronism
+{
+    public class CoroutineEngine
+    {
+        public bool _isRunning = true;
+
+        private readonly List<IEnumerator> _enumerators = new();
+
+        public CoroutineEngine()
+        {
+            ThreadPool.QueueUserWorkItem(Run);
+        }
+
+        private void ExecuteOneStep()
+        {
+            var index = 0;
+            while(index < _enumerators.Count)
+            {
+                if(!_enumerators[index].MoveNext())
+                {
+                    lock (_enumerators)
+                    {
+                        _enumerators.RemoveAt(index);
+                    }
+                }
+                else
+                {
+                    index++;
+                }
+            }
+        }
+
+        private void Run(object? _)
+        {
+            while(_isRunning)
+            {
+                ExecuteOneStep();
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void Stop()
+        {
+            _isRunning = false;
+        }
+
+        public void StartCoroutine(IEnumerator coroutine)
+        {
+            lock (_enumerators)
+            {
+                _enumerators.Add(coroutine);
+            }
+        }
+    }
+}
--- RubiNetwork22/Asynchronism/CoroutineSample.cs
+++ RubiNetwork22/Asynchronism/CoroutineSample.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Asynchronism
 {
-    class CoroutineSample
+    public static class CoroutineSample
     {
+        private static bool _programFinished = false;
+
+        private static IEnumerator MyCoroutine()
+        {
+            Console.WriteLine("1");
+            yield return null;
+            Console.WriteLine("2");
+            yield return null;
+            yield return null;
+            Console.WriteLine("3");
+            yield return null;
+            yield return null;
+            yield return null;
+            Console.WriteLine("4");
+            yield return null;
+            yield return null;
+            yield return null;
+            yield return null;
+            Console.WriteLine("Finished");
+            _programFinished = true;
+        }
+
+        public static void Run()
+        {
+            var coroutineEngine = new CoroutineEngine();
+            coroutineEngine.StartCoroutine(MyCoroutine());
+
+            while(!_programFinished)
+            {
+                Thread.Sleep(10);
+            }
+
+            coroutineEngine.Stop();
+        }
     }
 }
